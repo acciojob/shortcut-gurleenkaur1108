55a2806c00d3874c223fb6582081d131efee6787
gurleenkaur1108 <76990139+gurleenkaur1108@users.noreply.github.com>
2024-01-24 21:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function shortcut(s1, s2) {
-  // your code here
+  return s1.charAt(0) + s2.charAt(0);
 }
 
 // Do not change the code below.
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
